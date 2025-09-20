f94e7ec10955d3803cbcfe88a5f5f0059e990342
nalinwadhwa02 <nalin.wadhwa02@gmail.com>
2025-09-19 21:05:35 -0500

    adding hw 1 and 2

--- hw1/targeted_fgsm_1.py
+++ hw1/targeted_fgsm_1.py
@@ -17,37 +17,48 @@ N = nn.Sequential(nn.Linear(10, 10, bias=False),
 x = torch.rand((1,10)) # the first dimension is the batch size; the following dimensions the actual dimension of the data
 x.requires_grad_() # this is required so we can compute the gradient w.r.t x
 
-t = 1 # target class
+t = 1
 
-epsReal = 1.1375  #depending on your data this might be large or small
+epsReal = 1 #depending on your data this might be large or small
 eps = epsReal - 1e-7 # small constant to offset floating-point erros
 
-# The network N classfies x as belonging to class 2
-original_class = N(x).argmax(dim=1).item()  # TO LEARN: make sure you understand this expression
+original_class = N(x).argmax(dim=1).item() # TO LEARN: make sure you understand this expression
 print("Original Class: ", original_class)
 print("Scores: ", N(x).data)
 assert(original_class == 2)
 
-# compute gradient
-# note that CrossEntropyLoss() combines the cross-entropy loss and an implicit softmax function
+# targeted Iterative FGSM
+iterations = 100
+alpha = 0.25
+
 L = nn.CrossEntropyLoss()
-loss = L(N(x), torch.tensor([t], dtype=torch.long)) # TO LEARN: make sure you understand this line
-loss.backward()
 
-print("Loss: ", loss.data)
-print("Gradient: ", x.grad.data)
+adv_x = x.clone().detach()
+adv_x.requires_grad_(True)
 
+for iteration in range(iterations):
+    adv_x.requires_grad_(True)
+    N.zero_grad()
+    loss = L(N(adv_x), torch.tensor([t], dtype=torch.long))
+    loss.backward()
+    with torch.no_grad():
+        adv_x = adv_x - (alpha * adv_x.grad.sign())
+        # find delta, clamp with eps
+        delta = adv_x - x
+        delta = torch.clamp(delta, -eps, eps)
+        adv_x = x + delta
 
-# your code here
-# adv_x should be computed from x according to the fgsm-style perturbation such that the new class of xBar is the target class t above
-# hint: you can compute the gradient of the loss w.r.t to x as x.grad
-adv_x = x - eps * x.grad.sign()
+        # check if attack is successful
+        new_class = N(adv_x).argmax(dim=1).item()
+        if new_class == t:
+            break
 
+print(f"Iterations: {iteration+1}")
 new_class = N(adv_x).argmax(dim=1).item()
 print("New Class: ", new_class)
 print("Adv Scores: ", N(adv_x).data)
-assert(new_class == t)
+if new_class != t:
+    print("Failed to generate adversarial example")
 
-# it is not enough that adv_x is classified as t. We also need to make sure it is 'close' to the original x.
 print("Distance: ", torch.norm((x-adv_x),  p=float('inf')).data)
 assert( torch.norm((x-adv_x), p=float('inf')) <= epsReal)
